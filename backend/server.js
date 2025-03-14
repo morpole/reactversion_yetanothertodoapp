b763ee0da8adab843d1372a3ef85e07be016ed41
morpole <morpole@gmail.com>
2025-03-14 18:33:04 +0000

    removed serving of static files from server.js

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api/tasks', taskRoutes);
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
